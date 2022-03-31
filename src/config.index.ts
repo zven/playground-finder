3d08dd16d10bc0ceeacf1d6f09bcecee2f95fa22
gerrit:jdreyer <j.dreyer@fh-muenster.de>
2022-03-31 09:55:32 +0200

    Improved env-config script

--- src/config.index.ts
+++ src/config.index.ts
@@ -1,15 +1,21 @@
 import { writeFile } from 'fs'
 
-const targetPath = './src/environments/environment.prod.ts'
-const envConfigFile = `export const environment = {
-  production: true,
-  mapbox: {
-    token: '${process.env.MAPBOX_TOKEN}'
-  }
-};`
+const prodTargetPath = './src/environments/environment.prod.ts'
+const devTargetPath = './src/environments/environment.ts'
 
-writeFile(targetPath, envConfigFile, 'utf8', (err) => {
-  if (err) {
-    return console.log(err)
-  }
-})
+writeToPath(prodTargetPath)
+writeToPath(devTargetPath)
+
+function writeToPath(path) {
+  const envConfigFile = `export const environment = {
+    production: ${path === prodTargetPath},
+    mapbox: {
+      token: '${process.env.MAPBOX_TOKEN}'
+    }
+  };`
+  writeFile(path, envConfigFile, 'utf8', (err) => {
+    if (err) {
+      return console.log(err)
+    }
+  })
+}
