54d5925e2d59eed38ab9064883d14901781d0085
gerrit:jdreyer <j.dreyer@fh-muenster.de>
2022-04-06 11:16:43 +0200

    Updated icons and check location-options when using gps

--- src/app/service/location/location.service.ts
+++ src/app/service/location/location.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core'
 import { Geolocation, Position } from '@capacitor/geolocation'
 import { Platform } from '@ionic/angular'
+import { LocationOptionType } from '../location-management/location-management'
 import { LocationManagementService } from '../location-management/location-management.service'
 
 @Injectable({
@@ -12,40 +13,70 @@ export class LocationService {
   constructor(
     private locationManagementService: LocationManagementService,
     private platform: Platform
-  ) {}
-
-  async canUseLocation(requestPermission: boolean = false): Promise<Boolean> {
-    try {
-      var status = await Geolocation.checkPermissions()
-      if (
-        requestPermission &&
-        (status.location === 'prompt' || status.coarseLocation === 'prompt')
-      ) {
-        status = await Geolocation.requestPermissions()
-      }
-      return (
-        status.location === 'granted' || status.coarseLocation === 'granted'
-      )
-    } catch (e) {
-      const isApp = this.platform.is('android') || this.platform.is('ios')
-      return !isApp
-    }
-  }
-
-  async registerListener(callback: (location: Position) => void) {
-    if (this.callbackID) return
-    this.callbackID = await Geolocation.watchPosition(
-      {
-        enableHighAccuracy: true,
-      },
-      async (position: Position | null, err?: any) => {
-        const processedPosition =
-          await this.locationManagementService.processLocation(position)
-        if (processedPosition) {
-          callback(processedPosition)
+  ) {
+    this.locationManagementService.locationOptions.subscribe(
+      async (newOptions) => {
+        if (!newOptions || !newOptions.length) return
+        const canUseNavigation =
+          await this.locationManagementService.loadLocationOption(
+            LocationOptionType.navigation
+          )
+        if (!canUseNavigation) {
+          this.removeListener()
         }
       }
     )
+  }
+
+  async canUseLocation(requestPermission: boolean = false): Promise<Boolean> {
+    if (
+      (
+        await this.locationManagementService.loadLocationOption(
+          LocationOptionType.playgrounds
+        )
+      ).value
+    ) {
+      try {
+        var status = await Geolocation.checkPermissions()
+        if (
+          requestPermission &&
+          (status.location === 'prompt' || status.coarseLocation === 'prompt')
+        ) {
+          status = await Geolocation.requestPermissions()
+        }
+        return (
+          status.location === 'granted' || status.coarseLocation === 'granted'
+        )
+      } catch (e) {
+        const isApp = this.platform.is('android') || this.platform.is('ios')
+        return !isApp
+      }
+    }
+    return false
+  }
+
+  async registerListener(callback: (location: Position) => void) {
+    if (
+      (
+        await this.locationManagementService.loadLocationOption(
+          LocationOptionType.navigation
+        )
+      ).value
+    ) {
+      if (this.callbackID) return
+      this.callbackID = await Geolocation.watchPosition(
+        {
+          enableHighAccuracy: true,
+        },
+        async (position: Position | null, err?: any) => {
+          const processedPosition =
+            await this.locationManagementService.processLocation(position)
+          if (processedPosition) {
+            callback(processedPosition)
+          }
+        }
+      )
+    }
   }
 
   async getCurrentLocation(): Promise<Position> {
